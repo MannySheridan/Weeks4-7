6ea49cc26e15dce2b80403076820ded8496587b2
carrmanu <carrmanu@OA-S141-19>
2026-06-02 17:25:19 -0400

    Work done in Week 5 of class

---
+++ Assets/Script/Runner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    public float speed;
+
+    bool isMoving = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isMoving);
+        if (isMoving == true)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+    public void OnMoveClick()
+    {
+        isMoving = true;
+ 
+    }
+
+  
+
+
